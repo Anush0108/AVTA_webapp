9713de2eb671b4e66edaf3751874809bcfe61d9d
Anushka Karhadkar <124391010+Anush0108@users.noreply.github.com>
2023-10-06 03:09:33 +0530

    Update Header.js

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import {NavLink,Link} from "react-router-dom";
-import {GiShoppingBag} from "react-icons/gi";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
+
 const Header = () => {
+  const [auth, setAuth] = useAuth();
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user: null,
+      token: "",
+    });
+    localStorage.removeItem("auth");
+    toast.success("Logout Successfully");
+  };
   return (
     <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link to='/' className="navbar-brand">
-       🛍️ AVTA
-        </Link>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-      <li className="nav-item">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link to="/" className="navbar-brand">
+              🛒 Ecommerce App
+            </Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <NavLink to="/" className="nav-link ">
                   Home
                 </NavLink>
@@ -24,29 +44,43 @@ const Header = () => {
                   Category
                 </NavLink>
               </li>
-        <li className="nav-item">
-          <NavLink to='/register' className="nav-link" >
-            Register
-            </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/login' className="nav-link">
-            Login
-            </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/cart' className="nav-link">
-            Cart(0)
-            </NavLink>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-
+              {!auth?.user ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink to="/register" className="nav-link">
+                      Register
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/login" className="nav-link">
+                      Login
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      onClick={handleLogout}
+                      to="/login"
+                      className="nav-link"
+                    >
+                      Logout
+                    </NavLink>
+                  </li>
+                </>
+              )}
+              <li className="nav-item">
+                <NavLink to="/cart" className="nav-link">
+                  Cart (0)
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
 
-export default Header
+export default Header;
